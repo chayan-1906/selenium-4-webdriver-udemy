5f1fdd3e33b5dcc01bbf755074058577a4648ded
pdas9647 <padmanabhadas9647@gmail.com>
2022-10-30 18:38:26 +0530

    Section 6 completed

---
+++ CalcProj/src/Calculator.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.cricbuzz.com/");
+		// ChromeDriver chromeDriver = new ChromeDriver();
+	}
+
+}
