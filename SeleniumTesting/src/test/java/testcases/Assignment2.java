a8973ecf7f79873db061c6f91808b08957f75702
pdas9647 <padmanabhadas9647@gmail.com>
2022-11-21 23:04:34 +0530

    Section 13 Assignment-2 done

---
+++ SeleniumTesting/src/test/java/testcases/Assignment2.java
@@ -0,0 +1,32 @@
+package testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/*
+ * Navigate to qa.way2automation.com or https://way2automation.com/way2auto_jquery/index.php
+ * on the registration form enter name and click on submit
+ */
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		driver.get("https://way2automation.com/way2auto_jquery/index.php");
+		driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Padmanabha Das");
+		driver.findElement(By.xpath(
+				"//div[@id='load_box']//input[@value='Submit']"))
+				.click();
+	}
+
+}
