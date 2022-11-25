0c4b59f35eb8b76cfb0041039d97dac7ae07e8ff
pdas9647 <padmanabhadas9647@gmail.com>
2022-11-25 22:24:04 +0530

    Section 17 Video 159 completed

---
+++ SeleniumTesting/src/test/java/testcases/Assignment3.java
@@ -0,0 +1,29 @@
+package testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/*
+ * Navigate to https://timesofindia.indiatimes.com/poll.cms
+ * Handle the text captcha
+ * add the numbers
+ * handle the operators +, - and *
+ */
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		driver.get("https://timesofindia.indiatimes.com/poll.cms");
+	}
+
+}
