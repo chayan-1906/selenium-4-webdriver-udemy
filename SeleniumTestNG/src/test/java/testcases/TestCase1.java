bac83bcb9af97e5fc4044132b71eb94ffdfec1a2
pdas9647 <padmanabhadas9647@gmail.com>
2022-12-16 06:58:26 +0530

    Section 24 Video 219 completed, stucked in video 220
    Co-Authored-By: Padmanabha Das <82430454+chayan-1906@users.noreply.github.com>

---
+++ SeleniumTestNG/src/test/java/testcases/TestCase1.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+	@Test
+	public void doLogin() {
+		System.out.println ( "Executing login test" );
+	}
+
+	/*public static void main(String[] args) {
+
+	}*/
+}
